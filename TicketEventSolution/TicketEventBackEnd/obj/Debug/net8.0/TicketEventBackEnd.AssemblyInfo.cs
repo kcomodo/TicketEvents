ae4379b085d6517dd70c8b086833cb58a300fd65
Quang Ho <mxrshmalloww@gmail.com>
2024-09-02 16:07:16 -0400

    Created JWT token generator, will implement later.

--- TicketEventSolution/TicketEventBackEnd/obj/Debug/net8.0/TicketEventBackEnd.AssemblyInfo.cs
+++ TicketEventSolution/TicketEventBackEnd/obj/Debug/net8.0/TicketEventBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0898926493cf33277c76b82230b9161bad68c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b87acaeff61aa73310c533e46d5eeb8ecde37e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
