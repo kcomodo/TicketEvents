1353d34cc64e0f0763fd5cf339770ee0d78149bf
Quang Ho <mxrshmalloww@gmail.com>
2024-09-28 12:39:35 -0400

    Unit testing Finished unit testing section

--- TicketEventSolution/ConsoleTest/obj/Debug/net8.0/ConsoleTest.AssemblyInfo.cs
+++ TicketEventSolution/ConsoleTest/obj/Debug/net8.0/ConsoleTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20fb0bf47befe1b20b3a27b045c18867c234c01f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c22faeddc785055612f3dcb4216c9e62704a222")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TicketEventSolution/TicketEventBackEnd/obj/Debug/net8.0/TicketEventBackEnd.AssemblyInfo.cs
+++ TicketEventSolution/TicketEventBackEnd/obj/Debug/net8.0/TicketEventBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9dd59af1488656bd1f9bd1c9593b0aa7384912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c22faeddc785055612f3dcb4216c9e62704a222")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
