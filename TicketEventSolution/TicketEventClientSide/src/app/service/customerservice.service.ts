27669552168125f0f38dd599d1809b8cdc62d134
Quang Ho <mxrshmalloww@gmail.com>
2024-10-08 11:50:22 -0400

    Home page and back-end for login Worked on home page intro box, and worked on login page.

--- TicketEventSolution/TicketEventClientSide/src/app/service/customerservice.service.ts
+++ TicketEventSolution/TicketEventClientSide/src/app/service/customerservice.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { CookieService } from 'ngx-cookie-service'; //npm install ngx-cookie-service --save --force used for read, saet, and delete browser cookies
+import { tap } from 'rxjs/operators';
+import { Token } from '@angular/compiler';
+import { map } from 'rxjs/operators';
+import { response } from 'express';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable, empty } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerserviceService {
+  //default url for the api which is swagger
+  private token: string | null = null;
+  private isAuthenticated = false;
+  private baseUrl = "http://localhost:7240";
+  private tokenSaved = 'tokenSaved';
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+    const token = this.getToken();
+    const header = new HttpHeaders({})
+  }
+  validateLogin(email: string, password: string) //: Observable<boolean>
+  {
+    const url = `${this.baseUrl}/ValidateLogin?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+    return this.http.post<{ token: string }>(`${this.baseUrl}/ValidateLogin?email=${email}&password=${password}`, "")
+  }
+  registerCustomer() {
 
-  constructor() { }
+  }
+  updateCustomerInfo() {
+
+  }
+  getToken(): string | null {
+    //grab the token from the cookie service and then return it to be stored
+    //cookieService is a library that allows us to store the token
+    return this.cookieService.get(this.tokenSaved) || null;
+  }
 }
