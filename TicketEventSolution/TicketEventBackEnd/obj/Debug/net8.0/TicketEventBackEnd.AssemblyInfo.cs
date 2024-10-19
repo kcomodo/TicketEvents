278aa45088cd4ab7fa885cf0fb015e16d67f6a4a
Quang Ho <mxrshmalloww@gmail.com>
2024-10-19 18:10:53 -0400

    Profile page Minor changes to profile page, need to udpate ngModel to work properly, and need to implement api to retrieve data.

--- TicketEventSolution/TicketEventBackEnd/obj/Debug/net8.0/TicketEventBackEnd.AssemblyInfo.cs
+++ TicketEventSolution/TicketEventBackEnd/obj/Debug/net8.0/TicketEventBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7307999ff651cc8a3077d8382928b743246292c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec965d2185faeb032793e5abca86810b792d682")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketEventBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
